e353b03294247dcf5d499daf2e63b54c084fb27b
TufanRai <thulungtufan16@gmail.com>
2025-07-29 18:44:41 +0545

    updated about shree ram temple page's widht

--- src/components/about/aboutTemple/AboutTemple.tsx
+++ src/components/about/aboutTemple/AboutTemple.tsx
@@ -4,29 +4,31 @@ import TempleLook from "@/public/TempleLook.jpg";
 
 const AboutTemple = () => {
   return (
-    <div className="w-full h-screen py-10 flex flex-col items-center justify-center overflow-x-hidden overflow-y-auto">
-      <h1 className="font-bold text-lg mt-16 text-red-600 sm:px-16">
-        {AboutRamTemple.title}
-      </h1>
-      <div className="w-full px-8 sm:px-16 mt-4">
-        <div className="w-full h-screen sm:px-16 sm:py-8 sm:grid sm:grid-col-3 sm:gap-2">
-          <div className="max-w-[500px] w-full max-h-56 h-screen col-start-2 row-start-1 shadow-lg shadow-neutral-500 border overflow-hidden ease duration-350">
-            <img
-              src={`${TempleLook.src}`}
-              className="object-fill sm:hover:scale-110 ease duration-400"
-              width={"100%"}
-              alt=""
-            />
-          </div>
-          <div className="w-full">
-            {AboutRamTemple.paragraphs?.map((value, index) => (
-              <p
-                key={index}
-                className="font-light text-xs/5 sm:text-sm/5 text-neutral-800 py-2"
-              >
-                {value}
-              </p>
-            ))}
+    <div className="w-full h-screen flex items-center justify-center">
+      <div className="w-full h-screen py-10 flex flex-col items-center justify-center overflow-x-hidden overflow-y-auto">
+        <h1 className="font-bold text-lg mt-16 text-red-600 sm:px-16">
+          {AboutRamTemple.title}
+        </h1>
+        <div className="w-full px-8 sm:px-16 mt-4">
+          <div className="w-full h-screen md:px-16 md:py-8 md:grid md:grid-col-3 sm:gap-2">
+            <div className="max-w-[500px] w-full max-h-56 h-screen col-start-2 row-start-1 shadow-lg shadow-neutral-500 border overflow-hidden ease duration-350">
+              <img
+                src={`${TempleLook.src}`}
+                className="object-fill sm:hover:scale-110 ease duration-400"
+                width={"100%"}
+                alt=""
+              />
+            </div>
+            <div className="w-full">
+              {AboutRamTemple.paragraphs?.map((value, index) => (
+                <p
+                  key={index}
+                  className="font-light text-xs/5 sm:text-sm/5 text-neutral-800 py-2"
+                >
+                  {value}
+                </p>
+              ))}
+            </div>
           </div>
         </div>
       </div>
