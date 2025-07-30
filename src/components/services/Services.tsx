6ae6e1ae534bbf939804b7a7db6a7d2021650a5e
TufanRai <thulungtufan16@gmail.com>
2025-07-30 17:11:32 +0545

    updated the font sizes in the page

--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -4,7 +4,7 @@ import { OfferingsAndServices } from "@/src/contents/StaticContents";
 const Services = () => {
   return (
     <div className="w-full h-screen  px-8 py-4 sm:px-16">
-      <h1 className="font-semibold text-xl text-red-500 sm:mx-8">
+      <h1 className="font-semibold text-xl text-red-500 sm:mx-8 xl:text-2xl">
         Daily and Occasional Activities at Śrī Rāmachandra Temple, Battisputali
       </h1>
       <div>
@@ -13,13 +13,13 @@ const Services = () => {
             className="px-8 py-4 flex flex-col items-center justify-start"
             key={index}
           >
-            <h3 className="font-medium text-lg px-4 w-full text-amber-500 sm:mx-8">
+            <h3 className="font-medium text-lg px-4 w-full text-amber-500 sm:mx-8 xl:text-xl">
               {value.title}
             </h3>
             <div className="w-full sm:px-8">
               {value.paragraph.map((pg, index) => (
                 <p
-                  className="font-light text-xs/5 sm:text-sm/5 text-neutral-800 py-2"
+                  className="font-light text-xs/5 sm:text-sm/5 text-neutral-800 py-2 xl:text-lg/5"
                   key={index}
                 >
                   {pg}
