608ec0e38f4443bdc9330a6d150241e14a15f3d7
TufanRai <thulungtufan16@gmail.com>
2025-07-30 17:09:43 +0545

    updated the width in shree ram about page

--- src/components/services/ServicePage.tsx
+++ src/components/services/ServicePage.tsx
@@ -4,8 +4,8 @@ import Services from "../services/Services";
 
 const ServicePage = () => {
   return (
-    <div className="w-full h-screen">
-      <div className=" sm:max-w-[1100px] w-full h-screen overflow-x-hidden flex flex-col items-start justify-start overflow-y-auto gap-1">
+    <div className="w-full h-screen flex items-center justify-center">
+      <div className=" sm:max-w-[1920px] w-full h-screen overflow-x-hidden flex flex-col items-start justify-start overflow-y-auto gap-1">
         <Services />
       </div>
     </div>
