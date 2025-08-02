7f1101c840d292bfa6836025146cb3df06ddc168
TufanRai <thulungtufan16@gmail.com>
2025-08-02 21:28:25 +0545

    Updated the hero page

--- src/components/HeroPage/HeroPage.tsx
+++ src/components/HeroPage/HeroPage.tsx
@@ -5,15 +5,17 @@ import img1 from "@/public/shreeRamChandrarBanner.jpg";
 import img2 from "@/public/TempleLook.jpg";
 import img4 from "@/public/DeviteesOfShreeRam.jpg";
 import img3 from "@/public/ShreeRamDevotee2.jpg";
+import ThirdSectionPage from "../Hero component/ThirdSectionPage";
 
 const HeroPage = () => {
   const slides = [img1.src, img2.src, img3.src, img4.src];
   return (
-    <div className="max-w-[1820px] w-full flex flex-col items-center justify-start">
+    <div className="max-w-[1820px] w-full flex flex-col items-center justify-start overflow-x-hidden overflow-y-auto">
       <div className="w-full max-h-80 sm:max-h-screen overflow-hidden">
         <Carousel autoSlide slides={slides} />
       </div>
       <SecondSectionPage />
+      <ThirdSectionPage />
     </div>
   );
 };
--- src/components/about/aboutTemple/AboutTemple.tsx
+++ src/components/about/aboutTemple/AboutTemple.tsx
@@ -13,16 +13,16 @@ const AboutTemple = () => {
       }}
       className="w-full h-screen flex items-center justify-center overflow-hidden"
     >
-      <div className="w-full h-screen bg-black/45 sm:bg-linear-to-r sm:from-black/75 sm:to-white/0">
+      <div className="w-full h-screen overflow-y-auto bg-black/45 sm:bg-linear-to-r sm:from-black/75 sm:to-white/0">
         {AboutRamTemple.map((value, index) => (
-          <div key={index}>
+          <div key={index} className="mb-8">
             <h1 className="w-full px-8 py-2 font-bold text-lg mt-16 bg-red-600 text-white max-w-280 sm:ml-8 sm:px-16">
               {value.title}
             </h1>
-            <div className="w-full h-screen max-w-150 mt-4 sm:mt-8 sm:max-w-240 px-12 sm:px-16">
-              {value.paragraphs.map((paragraph, index) => (
+            <div className="w-full max-w-150 mt-4 sm:mt-8 sm:max-w-240 px-12 sm:px-16">
+              {value?.paragraphs.map((paragraph, key) => (
                 <p
-                  key={index}
+                  key={key}
                   className="font-medium text-sm/5 sm:text-md/5 text-white/75 sm:text-white py-2"
                 >
                   {paragraph}
@@ -37,3 +37,14 @@ const AboutTemple = () => {
 };
 
 export default AboutTemple;
+
+{
+  /* <h1 >
+              {value.title}
+            </h1>
+            <div >
+              {value.paragraphs.map((paragraph, index) => (
+                
+              ))}
+            </div> */
+}
