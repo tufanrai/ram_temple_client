847435cc0018c217b17d7464a443bdf6ff8e451f
TufanRai <thulungtufan16@gmail.com>
2025-08-03 07:57:22 +0545

    Updated the width of the query form

--- src/components/Query/QueryPage.tsx
+++ src/components/Query/QueryPage.tsx
@@ -17,9 +17,9 @@ const QueryPage = () => {
         className="w-full h-screen overflow-x-hidden flex flex-col items-center justify-start overflow-y-auto gap-1"
       >
         <div className="w-full h-screen bg-black/58 sm:bg-linear-to-r flex justify-center sm:from-black/45 sm:to-white/0">
-          <div className="max-w-[1280px] w-full flex flex-col-reverse items-center justify-start px-16 py-8 sm:flex-row sm:items-center sm:justify-center sm:px-8 sm:py-4">
+          <div className="max-w-[1280px] w-full flex flex-col-reverse items-center justify-start px-8 sm:px-16 py-8 sm:flex-row sm:items-center sm:justify-center sm:px-8 sm:py-4">
             <MediaLinks />
-            <div className="w-full flex flex-col rounded-sm border-[1px] border-black/24 bg-black/14 gap-3 shadow-lg py-4 px-16 sm:rounded-lg">
+            <div className="w-full flex flex-col rounded-sm border-[1px] border-black/24 bg-black/14 gap-3 shadow-lg py-4 px-8 sm:px-16 sm:rounded-lg">
               <h1 className="font-semibold text-xl border-b text-white py-1 text-center">
                 Query form
               </h1>
