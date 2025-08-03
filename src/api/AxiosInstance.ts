93f16d0bee579df34645543335a52f5b6edb8a35
TufanRai <thulungtufan16@gmail.com>
2025-08-03 14:04:30 +0545

    Updated the api responses

--- src/api/AxiosInstance.ts
+++ src/api/AxiosInstance.ts
@@ -1,6 +1,9 @@
+import "dotenv/config";
 import axios from "axios";
+
+const Uri = process.env.NEXT_PUBLIC_API ?? "";
 
 // axios instance
 export const axiosInstance = axios.create({
-  baseURL: "*",
+  baseURL: `${Uri}`,
 });
