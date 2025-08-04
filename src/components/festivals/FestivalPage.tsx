b27dabf0b43a6a318b96f715a3c1efdac4fb1b41
TufanRai <thulungtufan16@gmail.com>
2025-08-04 14:54:53 +0545

    updated the width in the festival page'

--- src/components/festivals/FestivalPage.tsx
+++ src/components/festivals/FestivalPage.tsx
@@ -15,22 +15,28 @@ const FestivalPage = () => {
         <h1 className="font-semibold text-lg text-white italic bg-linear-to-r from-green-800 to-green-950 px-8 sm:px-16 py-1">
           Festivals!
         </h1>
-        <div className="w-full border-b-1 border-slate-500 p-4">
+        <div className="w-full p-4">
           {Festivals.map((value, index) => (
             <div
-              className="w-full flex items-center justify-between cursor-pointer"
+              className="w-full flex items-center justify-between cursor-pointer border-b-1 border-slate-500 mt-4 py-2"
               onClick={() => SaveFestivalId(index)}
               key={index}
             >
-              <div className="max-w-[25%] h-26 px-4 py-1 overflow-hidden">
+              <div className="max-w-[45%] h-26 px-4 py-1 overflow-hidden">
                 <h3 className="font-bold text-lg">{value.title}</h3>
                 <p className="font-light text-sm text-slate-600/64">
                   {value.paragraph.at(0)?.split("")}
                 </p>
               </div>
-              <div className="max-w-50 rounded-md overflow-hidden">
-                <img src={value.image} width={"100%"} alt="" />
-              </div>
+              <div
+                style={{
+                  backgroundImage: `url('${value.image}')`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                  backgroundRepeat: "no-repeat",
+                }}
+                className="max-w-[30%] max-h-34 w-full h-screen border rounded-md overflow-hidden"
+              ></div>
             </div>
           ))}
         </div>
